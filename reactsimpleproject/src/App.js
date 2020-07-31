1890dadc0a96fd8c465647583294b320ebc4f2cb
yanyushan <yanyushan@outlook.com>
2020-07-31 10:29:11 +0800

    update

--- reactsimpleproject/src/App.js
+++ reactsimpleproject/src/App.js
@@ -1,44 +1,37 @@
 import React, {Component} from 'react';
 import axios from 'axios'
 import './App.css'
-import ItemTable from "./components/itemTable";
-import Filter from "./components/filter";
-import Form from "./components/form";
-import Button from "@material-ui/core/Button";
+
+import MaterialTable from 'material-table'
+import Container from '@material-ui/core/Container';
 
 //定义组件
 class App extends Component {
 //初始化状态
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            list: [{
-                id: '',
-                name: ''
-            }]
+            columns: [
+                {
+                    title: 'ID',
+                    field: 'id',
+                    type: 'numeric',
+                },
+                {
+                    title: 'Name',
+                    field: 'name',
+                },
+            ],
+            list: [
+                {
+                    id: '',
+                    name: '',
+                },
+            ],
         }
     }
 
-    render() {
-        console.log('Render App')
-        return (
-            <div className="container-fluid" style={{marginTop: '20px'}}>
-                <div className="row">
-                    {/*信息表格*/}
-                    <ItemTable
-                        list={this.state.list}
-                        deleteItem={this.deleteItem}
-                        changeItem={this.changeItem}
-                    />
-                    <Form myform={this.myForm}/>
-                    <Filter myfilter={this.myFilter}/>
-                </div>
 
-            </div>
-        );
-    }
-
-//
     componentDidMount() {
         let self = this;
         setTimeout(function () {
@@ -55,51 +48,57 @@ class App extends Component {
         })
     };
 
-    //修改
-    changeItem = (item) => {
-        axios.post(`/user/post`, {id: item.id, name: item.name}).then(({data}) => {
-            console.log(data);
-            let rowData = this.state.list;
-            for (let i in rowData) {
-                let unit = rowData[i]
-                if (unit.id === item.id) {
-                    console.log("change")
-                    rowData[i].name = item.name
-                }
-            }
-            this.setState({list: rowData});
-        })
-    };
+    render() {
+        return (
+            <Container maxWidth="lg">
+                <MaterialTable
+                    title="User Information"
+                    columns={this.state.columns}
+                    data={this.state.list}
+                    editable={{
+                        onRowAdd: (newData) =>
+                            new Promise((resolve) => {
+                                axios.post('/user/post', {name: newData.name})
+                                setTimeout(() => {
+                                    resolve();
+                                    this.setState((prevState) => {
+                                        const list = [...prevState.list];
+                                        list.push(newData);
+                                        return {...prevState, list};
+                                    });
+                                }, 600);
+                            }),
+                        onRowUpdate: (newData, oldData) =>
+                            new Promise((resolve) => {
+                                axios.post(`/user/post`, {id: newData.id, name: newData.name})
+                                setTimeout(() => {
+                                    resolve();
+                                    if (oldData) {
+                                        this.setState((prevState) => {
+                                            const list = [...prevState.list];
+                                            list[list.indexOf(oldData)] = newData;
+                                            return {...prevState, list};
+                                        });
+                                    }
+                                }, 600);
+                            }),
+                        onRowDelete: (oldData) =>
+                            new Promise((resolve) => {
+                                axios.get(`/user/delete`, {params: {id:oldData.id}})
+                                setTimeout(() => {
+                                    resolve();
+                                    this.setState((prevState) => {
+                                        const list = [...prevState.list];
+                                        list.splice(list.indexOf(oldData), 1);
+                                        return {...prevState, list};
+                                    });
+                                }, 600);
+                            }),
+                    }}
+                />
+            </Container>
 
-    //删除
-    deleteItem = (item) => {
-        axios.get(`/user/delete`, {params: {id: item.id}}).then(({data}) => {
-            console.log(data);
-            let rowData = this.state.list;
-            for (let i in rowData) {
-                let unit = rowData[i];
-                if (unit.id === item.id) {
-                    console.log("delete")
-                    delete rowData[i];
-                }
-            }
-            this.setState({list: rowData});
-        })
-    };
-
-    //提交
-    myForm = (name) => {
-
-        // 添加到comments中, 更新state
-        let items = this.state.list
-        let length =items.length
-        items[length]={id:length,name: name}
-        //items.unshift(item)
-        this.setState({list:items})
-    }
-
-    myFilter = (list) => {
-        this.setState({list})
+        )
     }
 }
 
