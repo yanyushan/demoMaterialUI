cc4bf966559ea0dcad3812af9a03ed1a76528caa
yanyushan <yanyushan@outlook.com>
2020-08-02 22:23:15 +0800

    update

--- backendcode/src/main/java/com/example/demo2/user/UserController.java
+++ backendcode/src/main/java/com/example/demo2/user/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo2.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
+import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +11,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger log = Logger.getLogger(UserController.class);
     @Autowired
     private UserRepository userRepository;
     //查
     @RequestMapping("/query")
     public List<User> getList() {
+        log.error("this is error test");
+        log.debug("this is debug test");
+        log.info("this is info test");
+        log.warn("this is warn test");
         return this.userRepository.findAll();
     }
 
