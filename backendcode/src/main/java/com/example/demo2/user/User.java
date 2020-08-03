6b73b6937778ea06226f798a4848cea9725740c8
yanyushan <yanyushan@outlook.com>
2020-08-03 17:35:11 +0800

    update：对前端报文处理

--- backendcode/src/main/java/com/example/demo2/user/User.java
+++ backendcode/src/main/java/com/example/demo2/user/User.java
@@ -10,10 +10,7 @@ import java.util.Date;
 
 @Table(name = "user")
 @Entity
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @ToString
 @Accessors(chain = true)
 public class User {
@@ -23,4 +20,37 @@ public class User {
     private Integer id;
     private String name;
     private Date birthday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public User() {
+    }
+
+    public User(Integer id, String name, Date birthday) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
--- backendcode/src/main/java/com/example/demo2/user/UserController.java
+++ backendcode/src/main/java/com/example/demo2/user/UserController.java
@@ -1,11 +1,17 @@
 package com.example.demo2.user;
 
+import com.alibaba.fastjson.JSON;
+import com.mysql.cj.x.protobuf.MysqlxExpr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 import org.apache.log4j.Logger;
+import com.alibaba.fastjson.JSONObject;
 
+import javax.xml.crypto.Data;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -17,23 +23,20 @@ public class UserController {
     //查
     @RequestMapping("/query")
     public List<User> getList() {
-        log.error("this is error test");
-        log.debug("this is debug test");
-        log.info("this is info test");
-        log.warn("this is warn test");
         return this.userRepository.findAll();
     }
 
 
     @RequestMapping("/post")
-    public Object saveUser(@RequestBody User user) {
+    public Object saveUser(@RequestBody JSONObject requestData) {
+        User user=parseUser(requestData);
         this.userRepository.save(user);
         return this.userRepository.findAll();
     }
     @RequestMapping("/delete")//删
-    public Object delUser(@RequestParam("id") Integer id
-    ) {
-        this.userRepository.deleteById(id);
+    public Object delUser(@RequestBody JSONObject requestData) {
+        User user=parseUser(requestData);
+        this.userRepository.delete(user);
         return this.userRepository.findAll();
     }
     @RequestMapping("/filter")
@@ -41,7 +44,37 @@ public class UserController {
         return this.userRepository.findBetween(lower,upper);
     }
 
+    private User parseUser(JSONObject requestData){
+        JSONObject jsonObj=requestData.getJSONObject("requestData").getJSONObject("body").getJSONObject("msg");
 
+        Integer id = null;
+        try {
+            id = jsonObj.getInteger("id");
+        } catch (Exception e) {
+            log.warn("Cannot find user id!"+e.toString());
+        }
+
+        String name= null;
+        try {
+            name = jsonObj.getString("name");
+            if(name.equals("")){
+                log.debug("Invalid user name!");
+            }
+        } catch (Exception e) {
+            log.warn("Cannot find user name!"+e.toString());
+        }
+
+        Date birthday= null;
+        try {
+            birthday = jsonObj.getDate("birthday");
+            if(birthday.after(new Date())){
+                log.debug("Invalid birthday!");
+            }
+        } catch (Exception e) {
+            log.warn("Cannot find user birthday!"+e.toString());
+        }
+        return new User(id,name,birthday);
+    }
 
 
 }
