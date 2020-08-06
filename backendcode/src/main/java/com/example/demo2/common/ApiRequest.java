d8292dd360e28cbdcafc949a0b1862cc57690ec3
yanyushan <yanyushan@outlook.com>
2020-08-06 15:34:33 +0800

    update

--- backendcode/src/main/java/com/example/demo2/common/ApiRequest.java
+++ backendcode/src/main/java/com/example/demo2/common/ApiRequest.java
@@ -5,8 +5,6 @@ import com.example.demo2.user.User;
 import com.example.demo2.user.UserController;
 import org.apache.log4j.Logger;
 
-import java.util.Date;
-
 public class ApiRequest {
     private static final Logger log = Logger.getLogger(UserController.class);
 
@@ -14,7 +12,6 @@ public class ApiRequest {
         User user=new User();
         try {
             JSONObject jsonObj = requestData.getJSONObject("requestData").getJSONObject("msg");
-
             user.setId(jsonObj.getInteger("id"));
             user.setName(jsonObj.getString("name").trim());
             user.setBirthday(jsonObj.getDate("birthday"));
